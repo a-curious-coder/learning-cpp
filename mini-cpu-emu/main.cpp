f737d207c35336711a2a5d9eff9f0520ce8e3815
a-curious-coder <callummclennan1@googlemail.com>
2026-01-05 23:29:56 +0000

    Refined design of cpu emulator

--- mini-cpu-emu/main.cpp
+++ mini-cpu-emu/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
-class CPU {
+std::string instruction_set[5] = {"LOAD", "ADD", "STORE", "SUB", "HALT"};
+enum opcode { load = 0, add = 1, store = 2, sub = 3, halt = 4 };
 
-  std::string instruction_set[5] = {"LOAD", "ADD", "STORE", "SUBTRACT", "HALT"};
+class CPU {
   // Accumulator (sticky note - holds previous value)
   int AC = 0;
   // Program Counter (increments to ensure free space of memory?)
@@ -12,55 +13,57 @@ public:
   bool running = true;
 
   // Constructor
-  CPU(int memory[64]) {
+  CPU(int memory[32]) {
+    std::cout << "PC\tISA\tVAL\tAC" << std::endl;
     // While cpu is running
     while (running) {
       int instruction = memory[PC];
-      std::string instruction_string = return_instruction_string(instruction);
-
-      int value = memory[PC + 1];
-      increment_program_counter();
+      PC++;
+      int value = memory[PC];
+      PC++;
+      if (PC >= 32)
+        running = false;
       // Decode
+      if (instruction == 0 && value == 0)
+        continue;
+
+      std::string instruction_string = get_instruction_string(instruction);
+      std::cout << "-------------------------------" << std::endl;
+      std::cout << PC << "\t" << instruction_string << "\t" << value << "\t"
+                << AC << std::endl;
+      std::cout << "\nExecute " << instruction_string << " given value "
+                << value << "\n"
+                << std::endl;
       switch (instruction) {
-      case 0:
-        std::cout << "LOAD" << std::endl;
+      case load:
+        AC = memory[value];
         break;
-      case 1:
-        func();
+      case add:
+        AC = AC + value;
         break;
-      case 2:
-        func2();
+      case store:
+        memory[value] = AC;
         break;
-      case 3:
-        func3();
+      case sub:
+        AC = AC - value;
         break;
-      case 4:
-        func4();
-        break;
-      default:
-        std::cout << "Yurt" << std::endl;
+      case halt:
         break;
       }
+      AC = value;
+      std::cout << PC << "\t" << instruction_string << "\t" << value << "\t"
+                << AC << std::endl;
     }
     std::cout << "CPU is dead - bye bye" << std::endl;
   }
 
 private:
-  void func() { std::cout << "ADD" << std::endl; }
-  void func2() { std::cout << "STORE" << std::endl; }
-  void func3() { std::cout << "SUBTRACT" << std::endl; }
-  void func4() {
-    std::cout << "EXIT" << std::endl;
-    running = false;
-  }
-  std::string return_instruction_string(int instruction) {
+  std::string get_instruction_string(int instruction) {
     return instruction_set[instruction];
   }
-  void increment_program_counter() { PC += 2; }
 };
 
 int main() {
-  enum opcode { load = 0, add = 1, store = 2, sub = 3, halt = 4 };
 
   int memory[64] = {load, 1, sub, 2, store, 6, load, 3};
   CPU cpu(memory);
